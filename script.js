dcdc10dc3938b275d1a7cd748835e3fcba4197aa
Nsymeon <122832219+Nsymeon@users.noreply.github.com>
2025-06-30 20:42:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ document.getElementById("predict-btn").addEventListener("click", async function 
 
 document.getElementById("pdf-btn").addEventListener("click", function () {
   if (!currentRisk) {
-  alert("Please run the prediction first.");
+    alert("Please run the prediction first.");
   return;
-      
+  }
   const { jsPDF } = window.jspdf;
   const doc = new jsPDF();
 
